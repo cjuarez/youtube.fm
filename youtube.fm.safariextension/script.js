418f09c2bb4d8a1d8b7f5a48d601293040c2edd6
Celso Ulises Juarez Ramirez <ulyman@gmail.com>
2012-05-19 22:43:42 -0500

    Update in songs inside an artist
    Updated to display the link properly in artist song list (since it has
    an extra div inside the td). Also added the artist name to the link for
    artist song.

--- youtube.fm.safariextension/script.js
+++ youtube.fm.safariextension/script.js
@@ -4,12 +4,17 @@
  * Contact: celsojuarez at gmail
  */
 function populateLinks() {
-	var songs = document.getElementsByClassName("subjectCell"),
-		i = 0;
+	var songs = (document.querySelectorAll('td.subjectCell > div').length > 0) ? document.querySelectorAll('td.subjectCell > div') : document.querySelectorAll('td.subjectCell'),
+		urlPath = document.location.pathname.split("/"),
+		i = 0,
+		artist = '';
+	if (urlPath[1] === "music") {
+		artist = urlPath[2] + '+';
+	}
 	for (i; i < songs.length; i++) {
 		var content = songs[i].innerHTML;
-		var searchText = songs[i].innerText;
-		var link = '<a class="youtube_link" href="http://www.youtube.com/results?search_query=' + searchText + '"></a>&nbsp;';
+		var searchText = songs[i].innerText.replace(/\s/g,'+');
+		var link = '<a class="youtube_link" href="http://www.youtube.com/results?search_query=' + artist + searchText + '"></a>&nbsp;';
 		content = link + content;
 		songs[i].innerHTML = content;
 	}
