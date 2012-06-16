02426e4c713951f0dec25aaa3ee96dce9fe98eef
Celso Ulises Juarez Ramirez <ulyman@gmail.com>
2012-06-15 23:45:43 -0500

    Bug fixes

--- youtube.fm.safariextension/script.js
+++ youtube.fm.safariextension/script.js
@@ -3,25 +3,86 @@
  * Author: Celso Ulises Juarez Ramirez (cjuarez)
  * Contact: celsojuarez at gmail
  */
-function populateLinks() {
-	var songsWithDiv = document.querySelectorAll('tbody td.subjectCell > div'),
-		songsNoDiv = document.querySelectorAll('tbody td.subjectCell'),
-		songs,
-		urlPath = document.location.pathname.split("/"),
-		i = 0,
-		artist = '';
-		songsWithDiv = Array.prototype.slice.call(songsWithDiv);
-		songsNoDiv = Array.prototype.slice.call(songsNoDiv);
-		songs = songsWithDiv.concat(songsNoDiv);
-	if (urlPath[1] === "music") {
-		artist = urlPath[2] + '+';
+(function () {
+	"use strict";
+	function joinNodeLists(nl1, nl2) {
+		var r = Array.prototype.slice.call(nl1),
+			i,
+			j,
+			found = false,
+			bigger,
+			smaller,
+			withDivs = '';
+		if (nl1.length === 0) {
+			r = Array.prototype.slice.call(nl2);
+		} else {
+			if (nl1.length > nl2.length) {
+				bigger = nl1;
+				smaller = nl2;
+				withDivs = 'bigger';
+			} else {
+				bigger = nl2;
+				smaller = nl1;
+				withDivs = 'smaller';
+			}
+			for (i = 0; i < bigger.length; i++) {
+				for (j = 0; j < smaller.length; j++) {
+					if (withDivs === 'bigger') {
+						bigger[i] = bigger[i].parentNode;
+					} else {
+						smaller[i] = smaller[i].parentNode;
+					}
+					if (bigger[i] === smaller[j]) {
+						found = true;
+						break;
+					}
+				}
+				if (!found) {
+					r.push(nl1[j]);
+				}
+			}
+		}
+		return r;
 	}
-	for (i; i < songs.length; i++) {
-		var content = songs[i].innerHTML;
-		var searchText = songs[i].innerText.replace(/\s/g,'+');
-		var link = '<a class="youtube_link" href="http://www.youtube.com/results?search_query=' + artist + searchText + '"></a>&nbsp;';
-		content = link + content;
-		songs[i].innerHTML = content;
+
+	function populateLinks() {
+		var songsWithDiv = document.querySelectorAll('tbody td.subjectCell > div'),
+			songsNoDiv = document.querySelectorAll('tbody td.subjectCell'),
+			songsResult = document.querySelectorAll('#trackResults td.track'),
+			songs,
+			urlPath = document.location.pathname.split("/"),
+			i = 0,
+			artist = '',
+			artistsNodeList = null,
+			content,
+			searchText,
+			link;
+		songs = joinNodeLists(songsWithDiv, songsNoDiv);
+		if (urlPath[1] === "music") {
+			artist = urlPath[2].replace('+',' ') + ' ';
+		}
+		for (i; i < songs.length; i++) {
+			content = songs[i].innerHTML;
+			searchText = songs[i].innerText;
+			link = '<a class="youtube_link" href="http://www.youtube.com/results?search_query=' 
+				+ encodeURIComponent(artist + searchText) 
+				+ '"></a>&nbsp;';
+			content = link + content;
+			songs[i].innerHTML = content;
+		}
+		if (songsResult.length > 0) {
+			artistsNodeList = document.querySelectorAll('#trackResults td.artist');
+			for (i = 0; i < songsResult.length; i++) {
+				content = songsResult.item(i).innerHTML;
+				searchText = songsResult.item(i).innerText;
+				artist = artistsNodeList.item(i).innerText;
+				link = '<a class="youtube_link" href="http://www.youtube.com/results?search_query=' 
+					+ encodeURIComponent(artist + ' ' + searchText) 
+					+ '"></a>&nbsp;';
+				content = link + content;
+				songsResult.item(i).innerHTML = content;
+			}
+		}
 	}
-}
-populateLinks();
+	populateLinks();
+}());
